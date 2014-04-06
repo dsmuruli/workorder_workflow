c080650f8be2bdc5b84f9d9a011bc9f21ea44104
isd <isd@localhost>
2014-04-05 17:44:35 -0700

    configuration for hibernate and json

--- woclient/src/com/dmuruli/wodemo/web/controller/WorkOrderController.java
+++ woclient/src/com/dmuruli/wodemo/web/controller/WorkOrderController.java
@@ -1,5 +1,22 @@
 package com.dmuruli.wodemo.web.controller;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.dmuruli.wodemo.entity.WorkOrder;
+@Controller
 public class WorkOrderController {
 
+	
+	public  @ResponseBody WorkOrder getSampleWorkOrder(){
+		WorkOrder workOrder = new WorkOrder();
+		workOrder.setContactName("Jos Schmoe");
+		workOrder.setDescription("Sample Work Order");
+		workOrder.setTitle("Test Work Order");
+		workOrder.setWorkOrderId(2323232311L);
+		return workOrder;
+	}
 }
---
+++ woclient/src/com/dmuruli/wodemo/web/view/WorkOrderList.java
@@ -0,0 +1,16 @@
+package com.dmuruli.wodemo.web.view;
+
+import java.util.ArrayList;
+
+import com.dmuruli.wodemo.entity.WorkOrder;
+
+public class WorkOrderList extends ArrayList<WorkOrder> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5876017325626607388L;
+
+	
+
+}
