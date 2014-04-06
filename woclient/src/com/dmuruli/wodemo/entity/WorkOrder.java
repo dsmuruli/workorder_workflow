df3e6b996c41ff6891be83e94a02c5486552c699
isd <isd@localhost>
2014-04-05 17:42:31 -0700

    persistence configuration settings

---
+++ woclient/src/com/dmuruli/wodemo/entity/WorkOrder.java
@@ -0,0 +1,96 @@
+package com.dmuruli.wodemo.entity;
+
+public class WorkOrder {
+	Long workOrderId;
+	String contactName;
+	String title;
+	String description;
+	
+	public Long getWorkOrderId() {
+		return workOrderId;
+	}
+	public void setWorkOrderId(Long workOrderId) {
+		this.workOrderId = workOrderId;
+	}
+	public String getContactName() {
+		return contactName;
+	}
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((contactName == null) ? 0 : contactName.hashCode());
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result
+				+ ((workOrderId == null) ? 0 : workOrderId.hashCode());
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		WorkOrder other = (WorkOrder) obj;
+		if (contactName == null) {
+			if (other.contactName != null) {
+				return false;
+			}
+		} else if (!contactName.equals(other.contactName)) {
+			return false;
+		}
+		if (description == null) {
+			if (other.description != null) {
+				return false;
+			}
+		} else if (!description.equals(other.description)) {
+			return false;
+		}
+		if (title == null) {
+			if (other.title != null) {
+				return false;
+			}
+		} else if (!title.equals(other.title)) {
+			return false;
+		}
+		if (workOrderId == null) {
+			if (other.workOrderId != null) {
+				return false;
+			}
+		} else if (!workOrderId.equals(other.workOrderId)) {
+			return false;
+		}
+		return true;
+	}
+	
+
+}
